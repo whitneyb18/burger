b7771b2154cca01bb3826145b5f5549d31d7e741
Gold Leader <whitneyb18@gmail.com>
2018-11-24 13:56:09 -0700

    Styling added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,8 +6,8 @@ var router = express.Router();
 var burgers = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burgers.all(function(data) {
+router.get("/", function (req, res) {
+  burgers.all(function (data) {
     var hbsObject = {
       burgers: data
     };
@@ -16,26 +16,27 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burgers.create(["name", "eaten"], [req.body.name, req.body.eaten], function(
+router.post("/api/burgers", function (req, res) {
+  burgers.create(["name", "eaten"], [req.body.name, req.body.eaten], function (
     result
   ) {
     // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+    res.json({
+      id: result.insertId
+    });
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  burgers.update(
-    {
+  burgers.update({
       eaten: req.body.eaten
     },
     condition,
-    function(result) {
+    function (result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
@@ -46,10 +47,10 @@ router.put("/api/burgers/:id", function(req, res) {
   );
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
+router.delete("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  burgers.delete(condition, function(result) {
+  burgers.delete(condition, function (result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
